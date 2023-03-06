f1ad8228a1d02e1a4eb9b3ed939daef1c9ce9f93
정인우 <inust33@gmail.com>
2023-03-06 20:56:25 +0900

    session

--- src/main/kotlin/com/example/numbletimedealserver/controller/CustomerController.kt
+++ src/main/kotlin/com/example/numbletimedealserver/controller/CustomerController.kt
@@ -6,15 +6,19 @@ import com.example.numbletimedealserver.request.LoginRequest
 import com.example.numbletimedealserver.request.SignUpRequest
 import com.example.numbletimedealserver.service.customer.CustomerService
 import com.example.numbletimedealserver.service.product.ProductService
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
 import jakarta.servlet.http.HttpSession
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.net.http.HttpResponse
 
 @RestController
 class CustomerController(private val productService: ProductService, private val customerService: CustomerService) {
@@ -26,10 +30,9 @@ class CustomerController(private val productService: ProductService, private val
         return customerService.signup(signupRequest).let{ResponseEntity.ok(it)}
     }
     @PostMapping("/login")
-    fun login(@RequestBody loginRequest: LoginRequest, httpSession: HttpSession):ResponseEntity<CustomerDto>{
+    fun login(@RequestBody loginRequest: LoginRequest, httpSession: HttpSession, response: HttpServletResponse):ResponseEntity<CustomerDto>{
         val loginresult=customerService.login(loginRequest)
-        httpSession.setAttribute("user", loginresult)
-        httpSession.maxInactiveInterval = 15000
+        httpSession.setAttribute("user", loginRequest)
         return ResponseEntity.ok(loginresult)
     }
     @PostMapping("/resign")
--- src/main/kotlin/com/example/numbletimedealserver/exception/CustomException.kt
+++ src/main/kotlin/com/example/numbletimedealserver/exception/CustomException.kt
@@ -3,7 +3,7 @@ package com.example.numbletimedealserver.exception
 sealed class CustomException(val code:ErrorCode) :RuntimeException(code.message) {
     class ProductNotFoundException:CustomException(ErrorCode.PRODUCT_NOT_FOUND)
     class NotLoggedInException:CustomException(ErrorCode.UNAUTHORIZED)
-    class UserExistsException:CustomException(ErrorCode.ALREADY_EXISTS_USER)
+    class UserConflictException:CustomException(ErrorCode.USER_CONFLICT)
     class UserNotFoundException:CustomException(ErrorCode.USER_NOT_FOUND)
     class BadRequestException:CustomException(ErrorCode.INVALID_PARAMETER)
     class ForbiddenException:CustomException(ErrorCode.FORBIDDEN)
